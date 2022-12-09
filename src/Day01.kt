4fdb77593fd67881f8b8dd6f09b1f162fb3fb482
Sahil Chhabra <sahil2921@gmail.com>
2022-12-09 18:28:39 +0000

    Day 1 Solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,35 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(inputs: List<String>): Int {
+        var max = 0
+        var sum = 0
+        for(input in inputs){
+            if(input == "") {
+                max = maxOf(max, sum)
+                sum = 0
+            } else {
+                sum += input.toInt()
+            }
+        }
+        return max
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(inputs: List<String>): Int {
+        val sums = ArrayList<Int>()
+        var sum = 0
+        for(input in inputs) {
+            if (input == "") {
+                sums.add(sum)
+                sum = 0
+            } else {
+                sum += input.toInt()
+            }
+        }
+        return sums.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
